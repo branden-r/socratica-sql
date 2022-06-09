a7abaea95903a536021f3da715a2996ac29824b3
bird.ron <bird.ron@pm.me>
2022-06-09 13:20:01 -0700

    Implemented questions ten and eleven.

---
+++ q10-cause.sql
@@ -0,0 +1,3 @@
+-- Question 10: Display every cause of these earthquakes.
+
+SELECT cause FROM earthquakes;
---
+++ q11-different-causes.sql
@@ -0,0 +1,5 @@
+-- Question 11: What are the different causes of these earthquakes?
+-- Each cause displayed in your answer should be unique.
+
+SELECT DISTINCT cause
+FROM earthquakes;
