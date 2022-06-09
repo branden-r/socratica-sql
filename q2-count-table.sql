f1b85795b16844ea66d4172dca163ea5397e9ee8
bird.ron <bird.ron@pm.me>
2022-06-09 12:34:47 -0700

    Implemented questions two and three.

---
+++ q2-count-table.sql
@@ -0,0 +1,2 @@
+-- Question 02: Count the number of rows in the table.
+SELECT COUNT(*) FROM earthquakes;
---
+++ q3-mag-place-occurred.sql
@@ -0,0 +1,2 @@
+-- Question 03: Display the magnitude, place, and date of occurrence for every earthquake.
+SELECT magnitude, place, occurred_on FROM earthquakes;
