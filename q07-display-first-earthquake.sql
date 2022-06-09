477c27356b43fc88c415def5f52cc3328382cec6
bird.ron <bird.ron@pm.me>
2022-06-09 13:06:54 -0700

    Implemented question seven.

---
+++ q07-display-first-earthquake.sql
@@ -0,0 +1,5 @@
+-- Question 07: Display the first earthquake listed in the table.
+-- This does not refer to chronological order, just the order in which the rows are stored in the table.
+
+SELECT * FROM earthquakes
+LIMIT 1;
